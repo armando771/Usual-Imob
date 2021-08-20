2179abf11381030538e143646ba2a95101524cd6
Armando <a.guerra58@hotmail.com>
2021-08-20 17:02:37 -0300

    Adicionando Estilizaçao nas page Sale & Rent

--- src/pages/RentPage.jsx
+++ src/pages/RentPage.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import '../styles/RentPage.css';
 
 function Rent() {
   return (
     <main>
       <Header />
-      <h3>My Page of Rent</h3>
+      <div id="Filter-Rent-Container">
+        <p>Encontre o imovel que a sua cara </p>
+        <label htmlFor="rent-filter-by-room">
+          <select id="rent-filter-by-room" className="form-select">
+            <option>Quartos</option>
+          </select>
+        </label>
+        <label htmlFor="rent-filter-by-price">
+          <select className="form-select">
+            <option>Preço</option>
+          </select>
+        </label>
+        <label htmlFor="rent-filter-by-place">
+          <select className="form-select">
+            <option>Cidade</option>
+          </select>
+        </label>
+        <button type="button" className="btn btn-secondary form-button">Filtrar</button>
+      </div>
       <Footer />
     </main>
   );
--- src/pages/SalePage.jsx
+++ src/pages/SalePage.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import '../styles/SalePage.css';
 
 function Sale() {
   return (
     <main>
       <Header />
-      <h3>My Page of Sale</h3>
+      <div id="Filter-Sale-Container">
+        <p>Encontre o imovel que a sua cara </p>
+        <label htmlFor="Sale-filter-by-room">
+          <select id="Sale-filter-by-room" className="form-select">
+            <option>Quartos</option>
+          </select>
+        </label>
+        <label htmlFor="Sale-filter-by-price">
+          <select className="form-select">
+            <option>Preço</option>
+          </select>
+        </label>
+        <label htmlFor="Sale-filter-by-place">
+          <select className="form-select">
+            <option>Cidade</option>
+          </select>
+        </label>
+        <button type="button" className="btn btn-secondary form-button">Filtrar</button>
+      </div>
       <Footer />
     </main>
   );
