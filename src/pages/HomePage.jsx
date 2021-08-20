ac3b88c014c3c733ebf65db5671e51dcda0d97c1
Wellington <junior.wel02@exemplo.br>
2021-08-20 17:03:27 -0300

    inicio do filtro na pagina inicial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import FilterHome from '../components/FilterHome';
 
 function HomePage() {
   return (
     <main>
       <Header />
       <h3>My HomePage</h3>
+      <FilterHome/>
       <Footer />
     </main>
   );
