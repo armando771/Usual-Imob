9c74631fe42e744ae72089c5990dbdacc4533060
Tulio <tuliotrindade99@gmail.com>
2021-08-20 17:04:40 -0300

    cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import casa from "../images/casa.jpg";
+import placeholder from "../images/placeholder.png"
+import home from "../images/home.png"
+import car from "../images/car.png"
+import "../styles/Card.css";
+
+
+function Cards() {
+	return(
+    <div className="cards">
+      <div className="card">
+        <div className="imagem">
+          <img src={ casa } className="casa" alt="casa" />
+        </div>
+        <div className="titulo">
+          <h1>APARTAMENTO - RESIDENCIAL</h1>
+        </div>
+        <div className="info">
+          <h2>VENDA</h2>
+          <p><img src={ placeholder } alt="adress"/>  Jaca - Rio de Janeiro/Rj</p>
+          <p><img src={ home } alt="home"/>  2 Dormitórios l 42m²</p>
+          <p><img src={ car } alt="car"/>  C/ Garagem</p>
+        </div>
+        <div className="button">
+          <button class="btn btn-secondary btn-lg" >VER MAIS</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="imagem">
+          <img src={ casa } className="casa" alt="casa" />
+        </div>
+        <div className="titulo">
+          <h1>CASA - RESIDENCIAL</h1>
+        </div>
+        <div className="info">
+          <h2>VENDA</h2>
+          <p><img src={ placeholder } alt="adress"/>  Jaca - Rio de Janeiro/Rj</p>
+          <p><img src={ home } alt="home"/>  2 Dormitórios l 42m²</p>
+          <p><img src={ car } alt="car"/>  C/ Garagem</p>
+        </div>
+        <div className="button">
+          <button class="btn btn-secondary btn-lg">VER MAIS</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="imagem">
+          <img src={ casa } className="casa" alt="casa" />
+        </div>
+        <div className="titulo">
+          <h1>CASA - RESIDENCIAL</h1>
+        </div>
+        <div className="info">
+          <h2>VENDA</h2>
+          <p><img src={ placeholder } alt="adress"/>  Jaca - Rio de Janeiro/Rj</p>
+          <p><img src={ home } alt="home"/>  2 Dormitórios l 42m²</p>
+          <p><img src={ car } alt="car"/>  C/ Garagem</p>
+        </div>
+        <div className="button">
+          <button class="btn btn-secondary btn-lg">VER MAIS</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="imagem">
+          <img src={ casa } className="casa" alt="casa" />
+        </div>
+        <div className="titulo">
+          <h1>CASA - RESIDENCIAL</h1>
+        </div>
+        <div className="info">
+          <h2>VENDA</h2>
+          <p><img src={ placeholder } alt="adress"/>  Jaca - Rio de Janeiro/Rj</p>
+          <p><img src={ home } alt="home"/>  2 Dormitórios l 42m²</p>
+          <p><img src={ car } alt="car"/>  C/ Garagem</p>
+        </div>
+        <div className="button">
+          <button class="btn btn-secondary btn-lg">VER MAIS</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="imagem">
+          <img src={ casa } className="casa" alt="casa" />
+        </div>
+        <div className="titulo">
+          <h1>CASA - RESIDENCIAL</h1>
+        </div>
+        <div className="info">
+          <h2>VENDA</h2>
+          <p><img src={ placeholder } alt="adress"/>  Jaca - Rio de Janeiro/Rj</p>
+          <p><img src={ home } alt="home"/>  2 Dormitórios l 42m²</p>
+          <p><img src={ car } alt="car"/>  C/ Garagem</p>
+        </div>
+        <div className="button">
+          <button class="btn btn-secondary btn-lg">VER MAIS</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Cards from '../components/Cards';
 
 function HomePage() {
   return (
     <main>
       <Header />
       <h3>My HomePage</h3>
+      <Cards />
       <Footer />
     </main>
   );
