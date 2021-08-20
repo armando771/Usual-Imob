7e79f216f0965195ed98990cf1e6f55687c50337
Tulio <tuliotrindade99@gmail.com>
2021-08-20 17:17:34 -0300

    cards finalizados, fix footer, fix filtro

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Cards from '../components/Cards';
+import FilterHome from '../components/FilterHome';
 
 function HomePage() {
   return (
     <main>
       <Header />
-      <h3>My HomePage</h3>
+      <FilterHome />
       <Cards />
       <Footer />
     </main>
