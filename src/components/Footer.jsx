70348dd9a1793dbbf698d74f8351af987d91a334
Armando <a.guerra58@hotmail.com>
2021-08-20 14:45:59 -0300

    Fazendo pequenas mudanças no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,17 +10,16 @@ function Footer() {
     <footer id="Footer-Main-Container">
       <div id="Footer-Container">
         <section id="section-hour">
-          <p id="section-hour-p"><img src={clock} alt="clock-icon_icone-de-relogio" /> Horário de Funcionamento</p>
+          <p id="section-hour-p"><img className="footer-images" src={clock} alt="clock-icon_icone-de-relogio" /> Horário de Funcionamento</p>
           <p id="section-hour-info">Segunda á Sexta 08h ás 18h </p>
         </section>
         <section id="section-adress">
-        <p id="section-adress-p"><img src={adress} alt="adress-icon_icone-de-endereço"/>Endereço:</p>
-          <span id="section-adress-street">Rua Extrema, 456</span>
-          <span id="section-adress-district">Guanabara, Contagem-MG</span>
+        <p id="section-adress-p"><img className="footer-images" src={adress} alt="adress-icon_icone-de-endereço"/>Endereço:</p>
+          <span id="section-adress-street">Rua Extrema, 456, Guanabara, Contagem-MG</span>
         </section>
         <section id="section-social">
-          <a id="section-social-facebook" rel="noreferrer" href="https://www.facebook.com/usualempreendimentosimobiliarios" target="_blank"> <img src={ facebook } alt="facebook-logo"/> </a>
-          <a href="https://www.instagram.com/usualimob/" rel="noreferrer" target="_blank"> <img src={ instagram } alt="instagam-logo"/> </a>
+          <a id="section-social-facebook" rel="noreferrer" href="https://www.facebook.com/usualempreendimentosimobiliarios" target="_blank"> <img className="footer-images" src={ facebook } alt="facebook-logo"/> </a>
+          <a href="https://www.instagram.com/usualimob/" rel="noreferrer" target="_blank"> <img className="footer-images" src={ instagram } alt="instagam-logo"/> </a>
         </section>
       </div>
     </footer>
