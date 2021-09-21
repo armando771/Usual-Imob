d8c45d5dded859f3d37d1872619fa09073cdd225
Armando <a.guerra58@hotmail.com>
2021-09-21 16:03:37 -0300

    object

---
+++ src/images/FotosIMoveis/EspacoComercialCabral/index.js
@@ -0,0 +1,23 @@
+import foto1 from './01.jpg'
+import foto2 from './02.jpg'
+import foto3 from './03.jpg'
+import foto4 from './04.jpg'
+import foto5 from './05.jpg'
+import foto6 from './06.jpg'
+import foto7 from './07.jpg'
+import foto8 from './08.jpg'
+import foto9 from './09.jpg'
+
+const EspacoComercialCabral = {
+  foto1,
+  foto2,
+  foto3,
+  foto4,
+  foto5,
+  foto6,
+  foto7,
+  foto8,
+  foto9,
+}
+
+export default EspacoComercialCabral;
