629544cedbbb239b097cf923cf75a35abecd14d5
Armando <a.guerra58@hotmail.com>
2021-09-26 15:40:18 -0300

    a

--- src/components/FooterSale.jsx
+++ src/components/FooterSale.jsx
@@ -7,20 +7,20 @@ import "../styles/FooterSale.css";
 
 function FooterSale() {
   return (
-    <footer className="Footer-Main-Container">
-      <div className="Footer-Container">
+    <footer className="Footer-Main-Container-Sale">
+      <div className="Footer-Container-Sale">
 
-        <section className="section-hour">
-          <p><img className="footer-images" src={ clock } alt="clock-icon_icone-de-relogio" /> Horário de Funcionamento</p>
-          <p id="hour-day-info">Segunda á Sexta 08h ás 18h </p> </section>
+        <section className="section-hour-Sale">
+          <p><img className="footer-images-Sale" src={ clock } alt="clock-icon_icone-de-relogio" /> Horário de Funcionamento</p>
+          <p id="hour-day-info-Sale">Segunda á Sexta 08h ás 18h </p> </section>
 
-        <section className="section-adress">
-          <p><img className="footer-images" id="adress-picture" src={adress} alt="adress-icon_icone-de-endereço"/>Endereço:</p>
-          <p id="address-street"><span id="address-street1">Rua Extrema, 456,</span><span>Guanabara, Contagem-MG</span></p> </section>
+        <section className="section-adress-Sale">
+          <p><img className="footer-images-Sale" id="adress-picture-Sale" src={adress} alt="adress-icon_icone-de-endereço"/>Endereço:</p>
+          <p id="address-street-Sale"><span id="address-street1-Sale">Rua Extrema, 456,</span><span>Guanabara, Contagem-MG</span></p> </section>
 
-        <section id="section-social">
-          <a rel="noreferrer" href="https://www.facebook.com/usualempreendimentosimobiliarios" target="_blank"> <img className="social-images" src={ facebook } alt="facebook-logo"/> </a>
-          <a href="https://www.instagram.com/usualimob/" rel="noreferrer" target="_blank"> <img className="social-images" src={ instagram } alt="instagam-logo"/> </a>
+        <section id="section-social-Sale">
+          <a rel="noreferrer" href="https://www.facebook.com/usualempreendimentosimobiliarios" target="_blank"> <img className="social-images-Sale" src={ facebook } alt="facebook-logo"/> </a>
+          <a href="https://www.instagram.com/usualimob/" rel="noreferrer" target="_blank"> <img className="social-images-Sale" src={ instagram } alt="instagam-logo"/> </a>
        </section>
 
       </div>
